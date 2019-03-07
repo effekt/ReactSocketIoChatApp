aeed2d4b395758e5d543f7c98e66aa3c1063bba9
Jesse Wheeler <jesse@i-leet.com>
2019-03-07 00:49:01 -0500

    Added missing require

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const mongoose = require('mongoose');
+const path = require('path');
 const port = process.env.PORT || 5000;
 const Message = require('./server/models/message');
 const Log = require('./server/models/log');
